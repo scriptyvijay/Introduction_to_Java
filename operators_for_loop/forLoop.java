4e3aae22670b66bee2877ef334e14f270ae4e9f2
Vijay Choudhary <scriptyvijay@gmail.com>
2022-05-07 18:08:02 +0530

    done

---
+++ operators_for_loop/forLoop.java
@@ -0,0 +1,9 @@
+package operators_for_loop;
+
+public class forLoop {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("I love Java");
+        }
+    }
+}
