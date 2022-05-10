e65242ef7f6f3b128f151beaa203ff9f7496f11d
Vijay Choudhary <scriptyvijay@gmail.com>
2022-05-10 19:47:53 +0530

    Changes

--- operators_for_loop/assignmentOperator.java
+++ operators_for_loop/assignmentOperator.java
@@ -3,6 +3,7 @@ package operators_for_loop;
 public class assignmentOperator {
     public static void main(String[] args) {
         int a = 10;
+        // All assignment operators
         a += 1; // a = a+1;
         a -= 1; // a = a-1;
         a *= 1; // a = a * 1;
